094c31f81c9fb2d8f9de26b3cf8a5708b36bb63d
Manaf <manaf.mohammed@amalitech.com>
2026-06-09 11:57:16 +0000

    test(conftest): use configure_spark_with_delta_pip to avoid Maven JAR download in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,19 +22,15 @@ def s3_client(aws_credentials):
 
 @pytest.fixture(scope="session")
 def spark():
+    from delta import configure_spark_with_delta_pip
     from pyspark.sql import SparkSession
 
-    session = (
+    builder = (
         SparkSession.builder.master("local[2]")
         .appName("ecommerce-lakehouse-test")
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-        .config(
-            "spark.sql.catalog.spark_catalog",
-            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-        )
         .config("spark.sql.shuffle.partitions", "4")
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0")
-        .getOrCreate()
+        .config("spark.ui.enabled", "false")
     )
+    session = configure_spark_with_delta_pip(builder).getOrCreate()
     yield session
     session.stop()
